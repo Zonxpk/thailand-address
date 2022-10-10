32e30bea5810d6dee23f6e93afcd117b9e3d494a
Xon <pakawat.smu@cp-origin.com>
2022-10-10 16:35:48 +0700

    fix: app unfined on ts

--- index.js
+++ index.js
@@ -2,19 +2,14 @@ let provinces = require("./address/provinces.json");
 let districts = require("./address/districts.json");
 let subDistricts = require("./address/sub_districts.json");
 
-app = {};
-
-app.getProvinces = () => {
-  return provinces;
+const app = {
+  getProvinces: () => provinces,
+  getDistricts: (provinceId) =>
+    districts.filter((data) => data.province_id == provinceId),
+  getSubDistricts: (districtId) =>
+    subDistricts.filter((data) => data.district_id == districtId),
+  getZipCodeBySubDistrict: (subDistrictId) =>
+    subDistricts.find((data) => data.id == subDistrictId)?.zip_code,
 };
-
-app.getDistricts = (provinceId) =>
-  districts.filter((data) => data.province_id == provinceId);
-
-app.getSubDistricts = (districtId) =>
-  subDistricts.filter((data) => data.district_id == districtId);
-
-app.getZipCodeBySubDistrict = (subDistrictId) =>
-  subDistricts.find((data) => data.id == subDistrictId)?.zip_code;
 
 module.exports = app;
